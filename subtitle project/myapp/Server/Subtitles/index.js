ff0fc50efc4790a507da27d029235279e31efd5d
yarden.regev <yardisome@gmail.com>
2022-02-19 16:10:28 +0200

    made subtitle database connection

---
+++ subtitle project/myapp/Server/Subtitles/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true })).use(bodyParser.json());
+
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://yardisome:Yardisome1@cluster0.sf8tv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL,{useNewUrlParser : true, useUnifiedTopology: true})
+.then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+.catch((error) => {console.log(error.message)});
+
