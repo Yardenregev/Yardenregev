5cbb4ccf621253902527ebc7bb213c5f8f7f5543
yarden.regev <yardisome@gmail.com>
2022-02-19 16:24:42 +0200

    created posts file, working

--- subtitle project/myapp/Server/Subtitles/index.js
+++ subtitle project/myapp/Server/Subtitles/index.js
@@ -3,7 +3,10 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
 const app = express();
+
+app.use('/posts',postRoutes);
 
 app.use(bodyParser.urlencoded({ extended: true })).use(bodyParser.json());
 
